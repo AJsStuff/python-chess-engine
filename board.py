7b176f4d2ab6d2fe11cdac5bcde4781f7b57736c
jamal <jamal.coleman.2008@gmail.com>
2024-05-26 11:32:13 -0400

    Added COMPLETE FEN notation support

--- board.py
+++ board.py
@@ -1,15 +1,26 @@
 from piece import *
 import math
 
+
+
 class Board:
+
+    STARTING_POSITION_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
     def __init__(self):
         self.rep = [[0 for file in range(8)] for rank in range(8)]
+
+        self.color_to_move = "white"
 
         self.white_castle_kingside: bool = True
         self.white_castle_queenside: bool = True
         self.black_castle_kingside: bool = True
         self.black_castle_queenside: bool = True
+
+        self.en_passant_square: int = -1
+
+        self.num_of_halfmoves: int = 0
+        self.num_of_moves: int = 0
     
 
     def __str__(self) -> str:
@@ -25,10 +36,12 @@ class Board:
 
         self.rep[rank][file] = piece.id
     
+    
     @classmethod
     def load_position_from_FEN(cls, fen: str):
         board = cls()
-        fen = fen.strip()
+        fen = fen.strip().split()
+        
 
         piece_dictionary = {
             "k" : King("black"),
@@ -48,7 +61,8 @@ class Board:
 
         rank, file = 0, 0
 
-        for char in fen:
+        # Placing Pieces
+        for char in fen[0]:
             if char == "/":
                 rank += 1
                 file = 0
@@ -64,9 +78,57 @@ class Board:
                     file += 1
 
 
+        # Color to move
+        board.color_to_move == "white" if fen[1] == "w" else "black"
+
+
+        # Castling rights
+        if fen[2] == "-":
+            board.white_castle_kingside = False
+            board.white_castle_queenside = False
+            board.black_castle_kingside = False
+            board.black_castle_queenside = False
+        else:
+            if "K" in fen[2]:
+                board.white_castle_kingside = True
+            
+            if "k" in fen[2]:
+                board.black_castle_kingside = True
+
+            if "Q" in fen[2]:
+                board.white_castle_queenside = True
+            
+            if "q" in fen[2]:
+                board.black_castle_queenside = True
+
+
+        # En Passant Squares
+        if "-" == fen[3]:
+            board.en_passant_square = -1
+        else:
+            file_dict = {
+                "a" : 0,
+                "b" : 1,
+                "c" : 2,
+                "d" : 3,
+                "e" : 4,
+                "f" : 5,
+                "g" : 6,
+                "h" : 7,
+            }
+
+            rank, file = 8 - int(fen[3][1]), file_dict[fen[3][0]]
+            board.en_passant_square = rank * 8 + file
+        
+
+        # Halfmoves & Fullmoves
+        board.num_of_halfmoves = int(fen[4])
+        board.num_of_moves = int(fen[5])
         return board
 
+    @classmethod
+    def load_starting_position(cls):
+        return cls().load_position_from_FEN(cls().STARTING_POSITION_FEN)
 
-board = Board.load_position_from_FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
-print(board)
+
         
